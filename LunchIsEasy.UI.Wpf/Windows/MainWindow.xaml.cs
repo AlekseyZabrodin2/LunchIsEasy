850ca18000ad136a8bf85ff234618d0aa4850144
Aleksey Zabrodin <rediskal50@gmail.com>
2023-04-14 15:43:22 +0300

    + Add ContentControl and Binding GetApplicationContent

--- LunchIsEasy.UI.Wpf/Windows/MainWindow.xaml.cs
+++ LunchIsEasy.UI.Wpf/Windows/MainWindow.xaml.cs
@@ -1,19 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace LunchIsEasy.UI.Wpf
+﻿namespace LunchIsEasy.UI.Wpf
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,11 +8,6 @@ namespace LunchIsEasy.UI.Wpf
         public MainWindow()
         {
             InitializeComponent();
-
-            Application.Current.MainWindow.Content = new PageAuthorization();
-
-            
-
         }
     }
 }
