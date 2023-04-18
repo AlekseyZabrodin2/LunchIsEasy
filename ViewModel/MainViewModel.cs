b7f3b481f55bc4a39799366889ea840d9fda2584
Aleksey Zabrodin <rediskal50@gmail.com>
2023-04-18 20:44:54 +0300

    + Add property for TextBox and Method for Registry new account

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,21 +1,33 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using ControlzEx.Standard;
 using LunchIsEasy.UI.Wpf.Model;
 using LunchIsEasy.UI.Wpf.Model.Data;
 using Microsoft.EntityFrameworkCore.Storage;
 using Prism.Commands;
 using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Navigation;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace LunchIsEasy.UI.Wpf
 {
     public class MainViewModel : BindableBase
     {
+
+        private string _name = "Name";
+        private string _surname = "Surname";
+        private string _telephone = "Telephone";
+        private string _login = "Login";
+        private string _password = "Password";
+        private string _repeatPassword = "Repeat password";
 
 
 
@@ -40,7 +52,93 @@ namespace LunchIsEasy.UI.Wpf
 
 
 
+        public string SetName
+        {
+            get
+            {
+                return _name;
+            }
 
+            set
+            {
+                SetProperty(ref _name, value);
+            }
+        }
+
+
+
+        public string SetSurname
+        {
+            get
+            {
+                return _surname;
+            }
+
+            set
+            {
+                SetProperty(ref _surname, value);
+            }
+        }
+
+
+
+        public string SetTelephone
+        {
+            get
+            {
+                return _telephone;
+            }
+
+            set
+            {
+                SetProperty(ref _telephone, value);
+            }
+        }
+
+
+
+        public string SetLogin
+        {
+            get
+            {
+                return _login;
+            }
+
+            set
+            {
+                SetProperty(ref _login, value);
+            }
+        }
+
+
+
+        public string SetPassword
+        {
+            get
+            {
+                return _password;
+            }
+
+            set
+            {
+                SetProperty(ref _password, value);
+            }
+        }
+
+
+
+        public string SetRepeatPassword
+        {
+            get
+            {
+                return _repeatPassword;
+            }
+
+            set
+            {
+                SetProperty(ref _repeatPassword, value);
+            }
+        }
 
 
 
@@ -54,6 +152,7 @@ namespace LunchIsEasy.UI.Wpf
         }
 
 
+
         private RelayCommand _toAuthorizationWindow;
         public RelayCommand ToAuthorizationWindow => _toAuthorizationWindow = new RelayCommand(PerformGoToAuthorizationWindow);
 
@@ -65,12 +164,30 @@ namespace LunchIsEasy.UI.Wpf
 
 
 
+        private RelayCommand _toRegisterAccount;
+        public RelayCommand ToRegisterAccount => _toRegisterAccount = new RelayCommand(PerformGoToRegisterAccount);
+
+        private void PerformGoToRegisterAccount()
+        {               
+            AccountDBCommand.RegisterAccount(_name, _surname, _telephone, _login, _password, _repeatPassword);
+
+            ClearFields();
+        }
 
 
 
+        private RelayCommand _toClearFields;
+        public RelayCommand ToClearFields => _toClearFields = new RelayCommand(ClearFields);
 
-
-
+        public void ClearFields()
+        {
+            SetName = "Name";
+            SetSurname = "Surname";
+            SetTelephone = "Telephone";
+            SetLogin = "Login";
+            SetPassword = "Password";
+            SetRepeatPassword = "Repeat password";
+        }
 
 
 
