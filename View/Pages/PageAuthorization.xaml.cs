f2e8330fac330526b54724e155179acf97e98e59
Aleksey Zabrodin <alekseyzabrodin2.0@gmail.com>
2023-04-24 15:32:07 +0300

    + Create PageAuthorization Design

--- View/Pages/PageAuthorization.xaml.cs
+++ View/Pages/PageAuthorization.xaml.cs
@@ -25,54 +25,5 @@ namespace LunchIsEasy.UI.Wpf
         {
             InitializeComponent();
         }
-
-
-
-
-
-
-        public void OnLoginTextBoxGotFocus(object sender, RoutedEventArgs e)
-        {
-            if (LoginTextBox.Text == "User Name")
-            {
-                LoginTextBox.Text = string.Empty;
-                LoginTextBox.Foreground = Brushes.Gray;
-                LoginTextBox.CaretIndex = 0;
-            }
-        }
-
-
-        public void OnLoginTextBoxLostFocus(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(LoginTextBox.Text))
-            {
-                LoginTextBox.Text = "User Name";
-                LoginTextBox.Foreground = Brushes.Gray;
-            }
-        }
-
-
-        public void OnPasswordTextBoxGotFocus(object sender, RoutedEventArgs e)
-        {
-            if (PasswordTextBox.Text == "Password")
-            {
-                PasswordTextBox.Text = string.Empty;
-                PasswordTextBox.Foreground = Brushes.Gray;
-                PasswordTextBox.CaretIndex = 0;
-            }
-        }
-
-
-        public void OnPasswordTextBoxLostFocus(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(PasswordTextBox.Text))
-            {
-                PasswordTextBox.Text = "Password";
-                PasswordTextBox.Foreground = Brushes.Gray;
-            }
-        }
-
-
-
     }
 }
